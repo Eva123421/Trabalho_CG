d16903fe09ce53f003c5725ab56bb3ff9a0b0f86
Eva123421 <gomes134marques@gmail.com>
2025-10-24 21:35:01 -0300

    Update parede.py

--- Trabalho_CG/parede.py
+++ Trabalho_CG/parede.py
@@ -9,6 +9,10 @@ class Parede:
         # Propriedades da parede
         self.pos=glm.vec3(0, 0, 0)
         self.scale=glm.vec3(2, 2, 2)
+        self.size = glm.vec3(abs(self.scale.x), 
+                             abs(self.scale.y), 
+                             abs(self.scale.z))
+
         self.color=glm.vec3(0.8, 0.2, 0.2)
 
         # Vértices da parede (um cubo)
@@ -54,50 +58,7 @@ class Parede:
 
         glBindBuffer(GL_ARRAY_BUFFER, 0)
         glBindVertexArray(0)
-
-    
-    # Colisão AABB com o jogador
-    def resolve_collision(self, player):
-
-        # Metade dos tamanhos
-        half_player = glm.vec3(0.5, 0.5, 0.5)
-        half_wall = self.scale / 2.0
-
-        # Posições mínimas e máximas
-        player_min = player.player_pos - half_player
-        player_max = player.player_pos + half_player
-        wall_min = self.pos - half_wall
-        wall_max = self.pos + half_wall
-
-        # Verifica interseção AABB
-        overlap_x = min(player_max.x, wall_max.x) - max(player_min.x, wall_min.x)
-        overlap_y = min(player_max.y, wall_max.y) - max(player_min.y, wall_min.y)
-        overlap_z = min(player_max.z, wall_max.z) - max(player_min.z, wall_min.z)
-
-        if overlap_x > 0 and overlap_y > 0 and overlap_z > 0:
-            # Descobre o eixo com menor sobreposição (menos invasão)
-            if overlap_y < overlap_x and overlap_y < overlap_z:
-                # Corrige no eixo Y (subida/descida)
-                if player.player_pos.y > self.pos.y:
-                    player.player_pos.y += overlap_y  # em cima da parede
-                    player.vel_y = 0
-                    player.on_ground = True
-                else:
-                    player.player_pos.y -= overlap_y  # empurrado por baixo
-                    player.vel_y = 0
-            elif overlap_x < overlap_z:
-                # Corrige no eixo X (lateral)
-                if player.player_pos.x > self.pos.x:
-                    player.player_pos.x += overlap_x
-                else:
-                    player.player_pos.x -= overlap_x
-            else:
-                # Corrige no eixo Z (frente/trás)
-                if player.player_pos.z > self.pos.z:
-                    player.player_pos.z += overlap_z
-                else:
-                    player.player_pos.z -= overlap_z
-    
+   
     #render da parede
     def render(self, shader):
         model = glm.mat4(1.0)
